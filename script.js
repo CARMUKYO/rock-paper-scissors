cc8fc1a55408cc7bcd26e4f0e8f33a1b2ebae106
Carmukyo <carlmike.aguila@gmail.com>
2023-06-09 10:13:03 +0800

    Adding ui

--- script.js
+++ script.js
@@ -1,56 +1,48 @@
+let playerScore = 0;
+let computerScore =0;
 let choice = ["ROCK", "PAPER", "SCISSORS"]; 
-let playerSelection;
 let computerSelection;
+const buttons = document.querySelectorAll('input');
 
-//get random index of array to get a random choice;
 function getComputerChoice(){
     return choice[Math.floor(Math.random()*3)];
 }
 
-//compare playerselection and computerselection to see if you win,lose or draw
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection){
+    let result = "";
+    computerSelection = getComputerChoice();
+
     if(playerSelection === computerSelection){
-        return "draw";
-   }else if(playerSelection === "ROCK" && computerSelection ==="PAPER"){
-        return "Win";
-   }else if(playerSelection === "ROCK" && computerSelection ==="SCISSORS"){
-        return "Lose";
-   }else if(playerSelection === "PAPER" && computerSelection ==="SCISSORS"){
-        return "Lose";
-   }else if(playerSelection === "PAPER" && computerSelection ==="ROCK"){
-        return "Win";
-   }else if(playerSelection === "SCISSORS" && computerSelection ==="ROCK"){
-        return "Lose";
-   }else if(playerSelection === "SCISSORS" && computerSelection === "PAPER"){
-        return "Win";
-   }
-}
+        result = ("Tie. You both chose "+ playerSelection + "<br> Player Score: " + playerScore + " | Computer score: " + computerScore);
+    }else if((playerSelection == 'ROCK' && computerSelection == 'SCISSORS') || (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') ||
+             (playerSelection == 'PAPER' && computerSelection == 'ROCK')){
+                playerScore += 1;
+                result = ('You win the round! ' + playerSelection + ' beats ' + computerSelection + "<br>Player score: " + playerScore + " | Computer score: " + computerScore);
+                if (playerScore == 5) {
+                    result += '<br>You won the game!';
+                    endGame();
+                }
+    }else{
+        computerScore += 1;
+        result = ('You lose the round! ' + computerSelection + ' beats ' + playerSelection + "<br>Player score: " + playerScore + " | Computer score: " + computerScore);
 
-//make the game five round, that keeps score and declares winner
-function game(){
-    let i = 0;
-    let playerScore=0;
-    let computerScore=0;
-    while(i<5){
-        playerSelection = prompt("Rock,Paper,Scissors","").toUpperCase();
-        computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection));
-        if(playRound(playerSelection,computerSelection) === "Win"){
-            playerScore++;
-        }else if(playRound(playerSelection,computerSelection) === "Lose"){
-            computerScore++;
+        if (computerScore == 5) {
+            result += '<br>You lost the game!';
+            endGame();
         }
-        i++;
     }
-
-    if(playerScore === computerScore){
-        console.log("draw");
-    }else if(playerScore > computerScore){
-        console.log("You WIN!" + playerScore + "-" + computerScore)
-    }else if(playerScore < computerScore){
-        console.log("You lose"+ playerScore + "-" + computerScore)
-    }
-    return "The game is done";
+    document.getElementById('result').innerHTML = result;
+    return;
 }
 
-console.log(game())
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value);
+    })
+})
+
+function endGame() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
